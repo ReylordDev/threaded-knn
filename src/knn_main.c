61c77c06f3f450d42bcb6187ea4eddddfea767c9
ReylordDev <luis.klocke@uni-bielefeld.de>
2022-07-16 22:55:40 +0200

    outline general pseudocode approach

--- src/knn_main.c
+++ src/knn_main.c
@@ -47,8 +47,20 @@ int main(int argc, char** argv) {
     int n_threads = (int) strtol(argv[5], NULL, 10);
     printf("fileName: %s, N: %ld, k_max: %d, B: %d, n_threads: %d\n", fileName, N, k_max, B, n_threads);
 
+    // read file contents
     FILE *file = malloc(sizeof (FILE*));
     readFile(fileName, file);
+
+    // split dataset into B sub sets
+        // same size if N mod B == 0, otherwise close to same size
+    // loop over k from 1 to k_max
+        // Rotate through sub sets, setting i as test set and the other B-1 sets as training set
+            // calculate which k neighbors are closest using squared euclidean distance
+                // use thread pool for this
+            // assign most common class among k neighbors to vector
+        // calculate classification quality: correct qualifications / all classifications
+        // print "%d %g\n", k, class_qual
+    // print k with optimal class_qual (no parallelization)
 
     return(0);
 }
