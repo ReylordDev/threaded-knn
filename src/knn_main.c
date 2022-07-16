0ac8a5cf2e19c9bb2f055b84cd9dbd41dd322c53
ReylordDev <luis.klocke@uni-bielefeld.de>
2022-07-17 00:18:08 +0200

    outline thread usage

--- src/knn_main.c
+++ src/knn_main.c
@@ -6,6 +6,30 @@
 
 #define BUFFER_SIZE 1000
 
+typedef struct {
+    // necessary information for managing worker threads
+} thread_pool_t;
+
+typedef struct {
+    // tba
+} Task;
+
+// create thread pool and start thread count worker threads
+void thread_pool_init(thread_pool_t* thread_pool, int thread_count);
+
+// pass pointer to function with args pointer, enqueue it in task list and signalize worker thread
+// args might contain a set of indices to data vectors to calculate
+// data type of arg can change depending on computation phase
+void thread_pool_enqueue(thread_pool_t* thread_pool, void *(*function) (void *), void* args);
+
+// blocking wait, until one of the threads completed a task.
+// returns the passed function and args of the enqueue call. args can contain resulting values
+Task* thread_pool_wait(thread_pool_t* thread_pool);
+
+// End all worker threads and free all allocated memory
+// called on end of program
+void thread_pool_shutdown(thread_pool_t* thread_pool);
+
 // error if printf or puts fails
 void handleOutputError() {
     exit(1);
@@ -61,6 +85,33 @@ int main(int argc, char** argv) {
         // calculate classification quality: correct qualifications / all classifications
         // print "%d %g\n", k, class_qual
     // print k with optimal class_qual (no parallelization)
+
+    // Parallelization
+    // n_threads number of worker threads
+    // created at the beginning of the program
+    // thread pool consists of 4 functions
+    // 3 computation phases, outside of pool, generalization is important here
+    // overlap is allowed here, if one task in phase 1 is complete, tasks in the next phase is allowed to start
+        // 1. Parallel computation of k_max nearest neighbors
+            // for all test sets
+                // distance of each vector of test set to all vectors in all training sets
+                // continuously build list, sorted by ascending distance, of k_max nearest neighbors for each vector in dataset
+                // this list can be used for all k's later
+        // 2. Parallel classification and scoring
+            // for set in B testsets
+                // classification of all test vectors in set
+                // for each k
+                    // which class is most common among k neighbors
+                    // on par: highest index wins
+                    // store the results in a fitting data-structure (?)
+            // think about efficiency for this phase
+        // 3. evaluation of a classification quality
+            // for each k
+                // compare classification of knn with original class
+                // calculate ratio: correct / all
+                // also in parallel
+        // results of these 3 phases can be stored in a single data structure, containing the data vectors and their classes,
+        // as well as information about the k_max nearest neighbors and the classification.
 
     return(0);
 }
