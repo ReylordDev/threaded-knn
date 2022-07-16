4ad7f9713ff77224f0c70c944d1583e48ca0bdc5
ReylordDev <luis.klocke@uni-bielefeld.de>
2022-07-16 11:26:34 +0200

    testing threads

--- src/knn_main.c
+++ src/knn_main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 // error if printf or puts fails
 void handleOutputError() {
@@ -12,13 +14,25 @@ void printUsage() {
     if (ret < 0) handleOutputError();
 }
 
+void* test(void *arg){
+    while(1) {
+        printf("yo\n");
+        sleep(1000);
+    }
+    return NULL;
+}
+
 int main(int argc, char** argv) {
     if (argc != 6) {
         printUsage();
         return 1;
     }
+    pthread_t newthread;
+
+    pthread_create(&newthread, NULL, test, NULL);
     for (int i = 1; i < argc; ++i) {
         printf("%s\n", argv[i]);
     }
+    pthread_join(newthread, NULL);
     return(0);
 }
