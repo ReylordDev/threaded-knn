88bf08fd564af9c5e6122dae36f1f8c9ab4e66b6
ReylordDev <luis.klocke@uni-bielefeld.de>
2022-07-20 22:44:56 +0200

    implement k max neighbor list, memory leaking included

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ C implemententation of cross-validated knn for operating systems course
 * make linked list for max k
 * If I'm done and don't need the normal data_set then refactor to initialize subsets directly from input file
 * check with big data if long conversions are bad
+* rename file name in comments and prints
+* valgrind needs to be run
+* save addresses or something in neighbor list
--- src/knn_main.c
+++ src/knn_main.c
@@ -7,6 +7,15 @@
 
 #define BUFFER_SIZE 1000
 
+struct list_head {
+    struct list_head *next, *prev;
+};
+
+struct neighbor_info{
+    struct list_head head;
+    double dist;
+};
+
 typedef struct {
     double *values;
     int dims;
@@ -15,7 +24,7 @@ typedef struct {
 typedef struct {
     vec_t vec;
     int class;
-    void *nearest_neighbors;
+    struct neighbor_info *neighbors;
 } data_vec_t;
 
 typedef struct {
@@ -47,6 +56,38 @@ Task* thread_pool_wait(thread_pool_t* thread_pool);
 // called on end of program
 void thread_pool_shutdown(thread_pool_t* thread_pool);
 
+/* initialize "shortcut links" for empty list */
+extern void
+list_init(struct list_head *head);
+
+/* insert new entry after the specified head */
+extern void
+list_add(struct list_head *new, struct list_head *head);
+
+/* insert new entry before the specified head */
+extern void
+list_add_tail(struct list_head *new, struct list_head *head);
+
+/* deletes entry from list and reinitialize it, returns pointer to entry */
+extern struct list_head*
+list_del(struct list_head *entry);
+
+/* delete entry from one list and insert after the specified head */
+extern void
+list_move(struct list_head *entry, struct list_head *head);
+
+/* delete entry from one list and insert before the specified head */
+extern void
+list_move_tail(struct list_head *entry, struct list_head *head);
+
+/* tests whether a list is empty */
+extern int
+list_empty(struct list_head *head);
+
+
+void
+print_list(struct list_head *head, int k_max);
+
 // error if printf or puts fails
 void handleOutputError() {
     exit(1);
@@ -76,7 +117,16 @@ void readInputData(FILE *file, data_set_t *data_set, int dims) {
     for (int i = 0; i < N; ++i) {
         fgets(buffer, BUFFER_SIZE, file);
         data_vec_t data_vec;
+
         data_vec.vec.dims = dims;
+
+        data_vec.neighbors = malloc(sizeof (struct neighbor_info));
+        data_vec.neighbors->dist = 0;
+        struct list_head *head = data_vec.neighbors;
+        list_init(head);
+        data_vec.neighbors->head = *head;
+//        printf("init anchor: %p, init next: %p\n", head, head->next);
+
         double *values = malloc(dims * sizeof(double));
         data_vec.vec.values = values;
         // parse buffer
@@ -127,6 +177,24 @@ double euclideanDistance(data_vec_t *test_vec, data_vec_t *train_vec) {
     return dist;
 }
 
+void sorted_insert(data_vec_t *test_vec, data_vec_t *train_vec, double distance, int k_max) {
+    struct list_head *anchor = &test_vec->neighbors->head;
+    struct list_head *current = anchor;
+    for (int i = 0; i < k_max; ++i) {
+        struct neighbor_info *next = current->next;
+//        printf("anchor: %p, current: %p, next: %p\n", anchor, current, current->next);
+//        printf("distance: %g, new_dist: %g, current: %p, next: %p\n", distance, next->dist , current, current->next);
+        if (distance <= next->dist || current->next == anchor) {
+            struct neighbor_info *new = malloc(sizeof (struct neighbor_info));
+            new->dist = distance;
+            list_add_tail(&new->head, current->next);
+            return;
+        } else {
+            current = current->next;
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc != 6) {
         printUsage();
@@ -157,6 +225,7 @@ int main(int argc, char** argv) {
 
     readInputData(file, &data_set, vec_dim);
     // data_set contains all N vectors
+    fclose(file);
 
     // split dataset into B sub sets
     data_set_t *sub_sets = malloc(B * sizeof(data_set_t));
@@ -190,26 +259,28 @@ int main(int argc, char** argv) {
         printf("test_set: %d\n", i);
         for (int j = 0; j < test_set.size; ++j) {
             data_vec_t data_vec = test_set.data[j];
-            printf("data_vector: %d\n", j);
+            printf("data_vector: %d, val[0]: %lg\n", j, data_vec.vec.values[0]);
             for (int k = 0; k < B; ++k) {
                 if (i == k) continue;
                 data_set_t training_set = sub_sets[k];
-                printf("training_set: %d\n", k);
+//                printf("training_set: %d\n", k);
                 for (int l = 0; l < training_set.size; ++l) {
                     data_vec_t training_data_vec = training_set.data[l];
-                    printf("training_data_vector: %d\n", l);
+//                    printf("training_data_vector: %d, val[0]: %lg\n", l, training_data_vec.vec.values[0]);
                     double dist = euclideanDistance(&data_vec, &training_data_vec);
-                    printf("distance: %lg\n", dist);
+//                    printf("distance: %lg\n", dist);
                     // continuously build list, sorted by ascending distance, of k_max nearest neighbors for each vector in dataset
+                    sorted_insert(&data_vec, &training_data_vec, dist, k_max);
                     // this list can be used for all k's later
                 }
 
             }
+            print_list(&data_vec.neighbors->head, k_max);
         }
     }
-        // 2. Parallel classification and scoring
-            // for set in B testsets
-                // classification of all test vectors in set
+    // 2. Parallel classification and scoring
+    // for set in B testsets
+    // classification of all test vectors in set
                 // for each k
                     // which class is most common among k neighbors
                     // on par: highest index wins
@@ -231,6 +302,85 @@ int main(int argc, char** argv) {
         free(sub_sets[i].data);
     }
     free(sub_sets);
-    fclose(file);
     return(0);
+}
+
+
+/* initialize "shortcut links" for empty list */
+void
+list_init(struct list_head *head)
+{
+    head->next = head;
+    head->prev = head;
+}
+
+/* insert new entry after the specified head */
+void
+list_add(struct list_head *new, struct list_head *head)
+{
+    new->next = head->next;
+    new->prev = head;
+    head->next->prev = new;
+    head->next = new;
+}
+
+/* insert new entry before the specified head */
+void
+list_add_tail(struct list_head *new, struct list_head *head)
+{
+    head->prev->next = new;
+    new->prev = head->prev;
+    head->prev = new;
+    new->next = head;
+}
+
+/* deletes entry from list and reinitialize it, returns pointer to entry */
+struct list_head*
+list_del(struct list_head *entry)
+{
+    entry->prev->next = entry->next;
+    entry->next->prev = entry->prev;
+    list_init(entry);
+    return entry;
+}
+
+/* delete entry from one list and insert after the specified head */
+void
+list_move(struct list_head *entry, struct list_head *head)
+{
+    list_del(entry);
+    list_add(entry, head);
+}
+
+/* delete entry from one list and insert before the specified head */
+void
+list_move_tail(struct list_head *entry, struct list_head *head)
+{
+    list_del(entry);
+    list_add_tail(entry, head);
+}
+
+/* tests whether a list is empty */
+int
+list_empty(struct list_head *head)
+{
+    if (head->next == head && head->prev == head) return 1;
+    return 0;
+}
+
+void
+print_list(struct list_head *head, int k_max)
+{
+    int empty = list_empty(head);
+    if (empty == 1) {
+        printf("Liste ist leer.\n");
+    } else {
+        printf("Liste:\n");
+        for (int i = 0; i <= k_max; ++i) {
+            struct neighbor_info *proc = head;
+            printf("%g\n", proc->dist);
+            head = head->next;
+        }
+        printf("---\n");
+    }
 }
